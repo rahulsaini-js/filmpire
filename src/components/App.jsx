edc5f2bbd4ab889f6a5778ce85ce530afacdba06
RAHUL SAINI <itsrahulsaini21@gmail.com>
2022-09-24 21:28:30 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom'
+import Actors from './Actors/Actors'
+import MovieInformation from './MovieInformation/MovieInformation'
+import Movies from './Movies/Movies'
+import NavBar from './NavBar/NavBar'
+import Profile from './Profile/Profile'
+
+import useStyles from './styles'
+
+
+const App = () => {
+  const classes=useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content} >
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path='/movie/:id'>
+            <MovieInformation />
+          </Route>
+          <Route exact path='/actors/:id'>
+            <Actors />
+          </Route>
+          <Route exact path='/'>
+            <Movies />
+          </Route>
+          <Route exact path='/profile/:id'>
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>Profile yo</div>
+  )
+}
+
+export default Profile
